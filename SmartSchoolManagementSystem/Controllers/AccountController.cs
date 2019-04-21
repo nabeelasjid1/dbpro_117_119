18f2c6e8508524bdfafc5ca910299d751502851b
Lionuncle <goninside@gmail.com>
2019-04-21 17:05:07 +0500

    1st Module Completed

--- SmartSchoolManagementSystem/Controllers/AccountController.cs
+++ SmartSchoolManagementSystem/Controllers/AccountController.cs
@@ -79,6 +79,23 @@ namespace SmartSchoolManagementSystem.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
+                    var user = await UserManager.FindAsync(model.Email, model.Password);
+                    if (UserManager.IsInRole(user.Id, "Admin"))
+                    {
+                        return RedirectToAction("Index", "Admin");
+                    }
+                    else if (UserManager.IsInRole(user.Id, "Student"))
+                    {
+                        return RedirectToAction("Index", "Student");
+                    }
+                    else if (UserManager.IsInRole(user.Id, "Parent"))
+                    {
+                        return RedirectToAction("Index", "Parent");
+                    }
+                    else if (UserManager.IsInRole(user.Id, "Teacher"))
+                    {
+                        return RedirectToAction("Index", "Teacher");
+                    }
                     return RedirectToLocal(returnUrl);
                 case SignInStatus.LockedOut:
                     return View("Lockout");
@@ -151,18 +168,18 @@ namespace SmartSchoolManagementSystem.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email, UserRole = model.UserRole };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
+                    await UserManager.AddToRoleAsync(user.Id, model.UserRole);
                     return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
--- SmartSchoolManagementSystem/Models/AccountViewModels.cs
+++ SmartSchoolManagementSystem/Models/AccountViewModels.cs
@@ -65,9 +65,17 @@ namespace SmartSchoolManagementSystem.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Select")]
+        public string UserRole { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
